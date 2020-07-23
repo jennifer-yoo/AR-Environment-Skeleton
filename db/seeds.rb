9a0052af44ceaee28a9f1a77d0c001be05dc7d44
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-07-23 17:26:40 -0400

    second deliverables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-require 'pry'
+#require 'pry'
 
-jenn = Student.create({first_name: "jennifer", last_name: "yoo", grade_level: "fifth"})
-andy = Student.create({first_name: "andrew", last_name: "smith", grade_level: "fourth"})
+jenn = Student.create(first_name: "jennifer", last_name: "yoo", grade_level: "fifth", teacher_id: 1)
+andy = Student.create(first_name: "andrew", last_name: "smith", grade_level: "fourth", teacher_id: 2)
 
 
 jones = Teacher.create({last_name: "Jones", grade_level: "fifth", years_of_experience: 5})
 taylor = Teacher.create({last_name: "Taylor", grade_level: "fourth", years_of_experience: 22})
 
-binding.pry
+#binding.pry
