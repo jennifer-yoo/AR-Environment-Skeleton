4c869a1e377a9be1676dfff4ac197114e0a464bc
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-07-23 16:49:43 -0400

    first deliverables done

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,11 @@
 class Teacher < ActiveRecord::Base
-    
-    def tenure
 
+    def tenure
+        if self.years_of_experience > 5
+            true
+        else 
+            false
+        end
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 
 jenn = Student.create({first_name: "jennifer", last_name: "yoo", grade_level: "fifth"})
 andy = Student.create({first_name: "andrew", last_name: "smith", grade_level: "fourth"})
@@ -6,3 +6,5 @@ andy = Student.create({first_name: "andrew", last_name: "smith", grade_level: "f
 
 jones = Teacher.create({last_name: "Jones", grade_level: "fifth", years_of_experience: 5})
 taylor = Teacher.create({last_name: "Taylor", grade_level: "fourth", years_of_experience: 22})
+
+binding.pry
